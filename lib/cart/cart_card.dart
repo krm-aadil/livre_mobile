1c28a70192a009ff8cebed721cc9be5c578a1eb1
Aadil <cb010188@students.apiit.lk>
2023-06-09 08:39:15 +0530

    foxed cart process

--- lib/cart/cart_card.dart
+++ lib/cart/cart_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'cart_merci.dart';
+
 class CartCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -8,26 +10,104 @@ class CartCard extends StatelessWidget {
         backgroundColor: Colors.white,
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
-          color: Colors.black,
+          color: Colors.teal,
           onPressed: () {
             Navigator.pop(context);
           },
         ),
         title: Text(
-          'Cart Card',
+          'Card Details',
           style: TextStyle(
-            color: Colors.black,
+            color: Colors.teal,
             fontSize: 20,
           ),
         ),
       ),
-      body: Center(
-        child: Text(
-          'Debit/Credit Card Selected',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-          ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: double.infinity,
+              height: 200,
+              decoration: BoxDecoration(
+                color: Colors.teal,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Image.asset(
+                'assets/login.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Card Number',
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'Expiration Date',
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 16),
+                      Expanded(
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            labelText: 'CVV',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Cardholder Name',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 24),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CartMerci(),
+                    ),
+                  );
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text(
+                    'Pay Now',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.teal,
+                  onPrimary: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
